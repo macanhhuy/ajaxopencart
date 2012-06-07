cdb3f0926352abf967d6f5050cdebd3ca5cb3862
Abhijeet Kinjawadekar <abhijeetgk@gmail.com>
2012-06-07 15:09:09 +0530

    Initial import for default opencart framework
    Signed-off-by: Abhijeet Kinjawadekar <abhijeetgk@gmail.com>

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart/');
+define('HTTP_IMAGE', 'http://localhost/opencart/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/admin/');
+define('HTTPS_IMAGE', 'http://localhost/opencart/image/');
+
+// DIR
+define('DIR_APPLICATION', 'E:\wamp\www\opencart/admin/');
+define('DIR_SYSTEM', 'E:\wamp\www\opencart/system/');
+define('DIR_DATABASE', 'E:\wamp\www\opencart/system/database/');
+define('DIR_LANGUAGE', 'E:\wamp\www\opencart/admin/language/');
+define('DIR_TEMPLATE', 'E:\wamp\www\opencart/admin/view/template/');
+define('DIR_CONFIG', 'E:\wamp\www\opencart/system/config/');
+define('DIR_IMAGE', 'E:\wamp\www\opencart/image/');
+define('DIR_CACHE', 'E:\wamp\www\opencart/system/cache/');
+define('DIR_DOWNLOAD', 'E:\wamp\www\opencart/download/');
+define('DIR_LOGS', 'E:\wamp\www\opencart/system/logs/');
+define('DIR_CATALOG', 'E:\wamp\www\opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dinesh');
+define('DB_PASSWORD', 'dinesh');
+define('DB_DATABASE', 'opencart_db');
+define('DB_PREFIX', '');
+?>
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/');
+define('HTTP_IMAGE', 'http://localhost/opencart/image/');
+define('HTTP_ADMIN', 'http://localhost/opencart/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/');
+define('HTTPS_IMAGE', 'http://localhost/opencart/image/');
+
+// DIR
+define('DIR_APPLICATION', 'E:\wamp\www\opencart/catalog/');
+define('DIR_SYSTEM', 'E:\wamp\www\opencart/system/');
+define('DIR_DATABASE', 'E:\wamp\www\opencart/system/database/');
+define('DIR_LANGUAGE', 'E:\wamp\www\opencart/catalog/language/');
+define('DIR_TEMPLATE', 'E:\wamp\www\opencart/catalog/view/theme/');
+define('DIR_CONFIG', 'E:\wamp\www\opencart/system/config/');
+define('DIR_IMAGE', 'E:\wamp\www\opencart/image/');
+define('DIR_CACHE', 'E:\wamp\www\opencart/system/cache/');
+define('DIR_DOWNLOAD', 'E:\wamp\www\opencart/download/');
+define('DIR_LOGS', 'E:\wamp\www\opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dinesh');
+define('DB_PASSWORD', 'dinesh');
+define('DB_DATABASE', 'opencart_db');
+define('DB_PREFIX', '');
+?>
